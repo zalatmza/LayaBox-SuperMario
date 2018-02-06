9fa80b20b0d93f964bb9cfc97dcd11de89d6ff23
weichen <weichen@bilibili.com>
2018-02-06 15:00:53 +0800

    + 修改动作

--- src/enginer/object/player.ts
+++ src/enginer/object/player.ts
@@ -5,13 +5,22 @@ import Base from './base'
 import render from '../render'
 const key = {
   left: 37,
+  up: 38,
   right: 39
 }
 
 export default class Player extends Base {
-  private runDir: 1 | -1 = 1 // 1 往右， -1 往左
-  private speed: number = 10
-  // private isRunning: boolean = false
+  // 移动方向1 往右， -1 往左
+  private runDir: 1 | -1 = 1
+  // 移动速度
+  private speedX: number = 10
+  // 跳跃中
+  private jumping: boolean = false
+  // 跳跃速度 = 下落速度
+  private speedY: number = -100
+  // 重力加速度
+  private acce: number = 10
+  // 当前帧
   private currentFrameIndex: number = 0
 
   private rightFrameList = [
@@ -27,25 +36,30 @@ export default class Player extends Base {
     require('./assets/player3.png')
   ]
 
+  // 键盘事件状态
   private keyState = Object.create(null)
 
   // 每帧动作
   private action (): void {
     const left: boolean = this.keyState[key.left]
     const right: boolean = this.keyState[key.right]
+    const up: boolean = this.keyState[key.up]
 
-    if (left && right) {
-      this.graphics.clear()
+    if (up && !this.jumping) {
+      this.jumping = true
+    }
+
+    if (left && right || !left && !right) {
       this.initAction()
     } else if (right) {
       this.graphics.clear()
-      this.x += this.speed
+      this.x += this.speedX
       this.runDir = 1
       this.loadImage(this.rightFrameList[this.currentFrameIndex % this.rightFrameList.length])
       this.currentFrameIndex ++
     } else if (left) {
       this.graphics.clear()
-      this.x -= this.speed
+      this.x -= this.speedX
       this.runDir = -1
       this.loadImage(this.leftFrameList[this.currentFrameIndex % this.leftFrameList.length])
       this.currentFrameIndex ++
@@ -59,8 +73,9 @@ export default class Player extends Base {
     })
 
     document.addEventListener('keyup', e => {
-      this.keyState[e.keyCode] = false
-      this.graphics.clear()
+      if (e.keyCode === key.left || e.keyCode === key.right) {
+        this.keyState[e.keyCode] = false
+      }
       this.currentFrameIndex = 0
       this.initAction()
     })
@@ -68,6 +83,7 @@ export default class Player extends Base {
 
   // 初始化角色动作
   private initAction (): void {
+    this.graphics.clear()
     if (this.runDir === 1) {
       this.loadImage(this.rightFrameList[3])
     } else if (this.runDir === -1) {
