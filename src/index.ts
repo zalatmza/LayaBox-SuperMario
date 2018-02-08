937589f43b5d85347d1a450d1e5c03dfdd405d79
HarryYin91610 <HarryYin91610@outlook.com>
2018-02-08 14:05:50 +0800

    背景移动

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 /**
  * Created by wconisan on 2018/2/1.
  */
+import { Background } from './enginer/background'
 import Player from './enginer/object/player'
 import { stageSize } from './enginer/const'
 import { preRender } from './enginer/render'
+
 // 桢率
 const FPS = 60
 // 程序入口
 class GameMain {
+  // 主角
   private player: Player
+  // 背景
+  private background: Background
+  // 游戏入口类构造函数
   constructor () {
     Laya.init(stageSize.width, stageSize.height)
     Laya.loader.load('./static/res/player.json',
@@ -16,14 +22,23 @@ class GameMain {
                     null,
                     Laya.Loader.ATLAS)
   }
+  // 动画资源加载完成处理函数
   private onLoaded (): void {
+    this.background = new Background()
+    Laya.stage.addChild(this.background)
     this.player = new Player(0, 400)
     Laya.stage.addChild(this.player)
     // preRender()
     Laya.timer.frameLoop(1, this, this.onLoop)
   }
+  // 游戏主循环
   private onLoop () {
     this.player.action()
+    this.background.x -= 10
+    if (this.background.x <= -4000) {
+      this.background.x = -4000
+    }
   }
 }
+// 启动游戏
 new GameMain()
