7c7d320d086ab5ff30d9fdcabfd8b730c3cea9a3
weichen <weichen@bilibili.com>
2018-02-08 16:20:12 +0800

    + player 运动

---
+++ src/enginer/const/index.ts
@@ -0,0 +1,49 @@
+/**
+ * Created by wconisan on 2018/2/6.
+ */
+export const stageSize = {
+  width: 1000,
+  height: 600
+}
+
+export const gameSize = {
+  width: 5000,
+  height: stageSize.height
+}
+
+// 马里奥属性
+export const playerProp = {
+  // 移动速度
+  speedX: 8,
+  // 跳跃初速度
+  speedY: -33,
+  // （假）重力加速度
+  acce: 2,
+  action: {
+    left: 'moveLeft',
+    right: 'moveRight'
+  }
+}
+
+// 主角尺寸
+export const playerSize = {
+  width: 50,
+  height: 60
+}
+
+// 障碍物尺寸
+export const blockSize = {
+  floorSize: {
+    width: playerSize.width,
+    height: 140
+  },
+  pipeSize: {
+    width: 70
+  }
+}
+
+export const key = {
+  left: Laya.Keyboard.LEFT,
+  up: Laya.Keyboard.UP,
+  right: Laya.Keyboard.RIGHT
+}
--- src/enginer/object/player.ts
+++ src/enginer/object/player.ts
@@ -2,82 +2,23 @@
  * Created by wconisan on 2018/2/5.
  */
 import Base from './base'
-import { stageSize, gameSize, playerSize } from '../../enginer/const'
-
-const key = {
-  left: Laya.Keyboard.LEFT,
-  up: Laya.Keyboard.UP,
-  right: Laya.Keyboard.RIGHT
-}
-
-const floorLevel = 400
+import { playerSize, playerProp } from '../../enginer/const'
 
 export default class Player extends Base {
   // 身体动画
-  private body: Laya.Animation
-  // 相对舞台的位置
-  private stageX: number = stageSize.width
+  public body: Laya.Animation
   // 移动方向1 往右， -1 往左
-  private runDir: 1 | -1 = 1
+  public runDir: 1 | -1 = 1
   // 移动速度
-  private speedX: number = 10
+  public speedX: number = playerProp.speedX
   // 跳跃中
-  private jumping: boolean = false
+  public jumping: boolean = false
   // 跳跃速度 = 下落速度
-  private speedY: number = -33
+  public speedY: number = playerProp.speedY
   // 重力加速度
-  private acce: number = 2
+  public acce: number = playerProp.acce
   // 键盘事件状态
-  private keyState = Object.create(null)
-
-  // 每帧动作
-  public getStageX (): number {
-    const left: boolean = this.keyState[key.left]
-    const right: boolean = this.keyState[key.right]
-    const up: boolean = this.keyState[key.up]
-
-    if (up && !this.jumping) {
-      this.jumping = true
-    }
-
-    if (this.jumping) {
-      const newSpeedY = this.speedY + this.acce
-      this.y = Math.round(this.y + (this.speedY + newSpeedY)/2)
-      this.speedY = newSpeedY
-      // floor需要加入碰撞检测
-      this.y = Math.max(0 ,Math.min(this.y, floorLevel))
-      if (this.y === floorLevel) {
-        this.jumping = false
-        this.speedY = -33
-      }
-    }
-
-    if (left && right || !left && !right) {
-      this.initAction()
-    } else if (right) {
-      if (this.stageX >= gameSize.width - stageSize.width / 2) {
-        this.stageX = Math.min(this.stageX, gameSize.width - stageSize.width / 2)
-        this.x = Math.min(this.x + this.speedX, stageSize.width - playerSize.width)
-      } else {
-        this.x = Math.min(this.x + this.speedX, stageSize.width / 2)
-        if (this.x === stageSize.width / 2) {
-          this.stageX += this.speedX
-        }
-      }
-      this.runDir = 1
-      if (!(this.body.isPlaying && this.body._actionName === 'moveRight')) {
-        this.playAnimation('moveRight')
-      }
-    } else if (left) {
-      this.x -= this.speedX
-      this.x = Math.max(this.x, 0)
-      this.runDir = -1
-      if (!(this.body.isPlaying && this.body._actionName === 'moveLeft')) {
-        this.playAnimation('moveLeft')
-      }
-    }
-    return this.stageX
-  }
+  public keyState = Object.create(null)
 
   // 初始化键盘事件
   private initEvent () {
@@ -91,7 +32,7 @@ export default class Player extends Base {
   }
 
   // 初始化角色动作
-  private initAction (): void {
+  public initAction (): void {
     this.body.clear()
     this.graphics.clear()
     if (this.runDir === 1) {
@@ -101,14 +42,16 @@ export default class Player extends Base {
     }
   }
 
-  private initAnimation (): void {
-    Laya.Animation.createFrames(['player/player0.png', 'player/player1.png', 'player/player2.png'], 'moveRight')
-    Laya.Animation.createFrames(['player/player3.png', 'player/player4.png', 'player/player5.png'], 'moveLeft')
+  public initAnimation (): void {
+    Laya.Animation.createFrames(['player/player0.png', 'player/player1.png', 'player/player2.png'],
+      playerProp.action.right)
+    Laya.Animation.createFrames(['player/player3.png', 'player/player4.png', 'player/player5.png'],
+      playerProp.action.left)
     this.body = new Laya.Animation()
     this.addChild(this.body)
   }
 
-  private playAnimation (actionName) {
+  public playAnimation (actionName) {
     this.graphics.clear()
     this.body.clear()
     this.body.play(0, true, actionName)
