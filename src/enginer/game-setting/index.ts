34468f88f96f63c446e14ac9bb048a16e305ae84
weichen <weichen@bilibili.com>
2018-02-09 14:33:19 +0800

    + 左右碰检

--- src/enginer/game-setting/index.ts
+++ src/enginer/game-setting/index.ts
@@ -4,8 +4,17 @@ import { blockSize, gameSize } from '../const'
 function initFloorArray (): Array<Floor> {
   const fnum = Math.floor(gameSize.width / blockSize.floorSize.width) + 1
   const fArray = []
-  for (let i = 0; i < fnum; i++) {
+  for (let i = 0; i < 6; i++) {
     fArray.push(new Floor(blockSize.floorSize.width * i, 460))
+  }
+  for (let i = 6; i < 8; i++) {
+    fArray.push(new Floor(blockSize.floorSize.width * i, 360))
+  }
+  for (let i = 8; i < 20; i++) {
+    fArray.push(new Floor(blockSize.floorSize.width * i, 460))
+  }
+  for (let i = 20; i < fnum; i++) {
+    fArray.push(new Floor(blockSize.floorSize.width * i, 360))
   }
   return fArray
 }
