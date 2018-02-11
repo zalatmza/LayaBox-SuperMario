adbf4a36949f3cb6ed37e0ab6104da81f04a8573
weichen <weichen@bilibili.com>
2018-02-11 14:08:02 +0800

    + update crash

--- src/index.ts
+++ src/index.ts
@@ -63,8 +63,8 @@ class GameMain {
               console.log('left')
               this.player.crashHandle(crashDir.left, item)
             case 2:
-              console.log('')
-              this.player.crashHandle(crashDir.up, item)
+              console.log('down')
+              this.player.crashHandle(crashDir.down, item)
               break
             case 3:
               console.log('right')
